1129eedfe4bb7534cf97e1cc2bdab22f69784ded
BadgerWIVV <marx.jared@gmail.com>
2014-10-25 15:42:57 -0500

    My cached-inverse functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will find the inverse of invertible matrices and store their
+## inverses to reduce calculation time in the future.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns functions to allow the cacheSolve function to
+## access cached inverses of matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #create a variable to store the inverse and initialize it with a NULL
+  m <- NULL
+  
+  #Create a function to store the matrix being inverted
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  #Create a function to recall the matrix
+  get <- function() x
+  
+  #Create a function that creates the invertible matrix
+  setsolve <- function(solve) m <<- solve
+  
+  #Create a function the calls the inverse matrix
+  getsolve <- function() m
+  
+  #List the functions that can be called for other functions
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function will either produce the previously calculated inverse of a matrix
+## or if it has not yet been calculated, it will find the inverse and produce the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #Recall any previous inverse solutions and store them as m
+  m <- x$getsolve()
+  
+  #Return the previously calculated matrix from the cache if its available
+  if(!is.null(m)) {
+    print("getting cached data")
+    return(m)
+  }
+  
+  #Recall the matrix and label it data
+  data <- x$get()
+  
+  #Calculate the inverse of the matrix and store as m
+  m <- solve(data, ...)
+  
+  #Store the inverse of data for future use in the cache
+  x$setsolve(m)
+  
+  #Display the inverse
+  m
 }
